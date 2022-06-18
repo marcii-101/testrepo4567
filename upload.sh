ae4165b8fcadd4e3851dd0e418c3f9c3137b18b9
marcii-101 <86745494+marcii-101@users.noreply.github.com>
2022-06-18 13:27:25 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,7 +2,9 @@
 set -e
 git config --global user.email "noone@example.com"
 git config --global user.name "Actions Build"
-mv main.sh.x main
-git add main
+mkdir -p build/
+mv main.sh.x build/main
+sha256sum build/main > build/main.sha256sum
+git add build/main
 git commit -m "Add build on $(date)"
 git push
